7fe037207f484b1c6ed98823605b964a3deded6d
Gulay <gulaynasibova28@gmail.com>
2024-11-24 13:22:25 +0400

    Js-ders6
    JS-ders6

---
+++ app.js
@@ -0,0 +1,43 @@
+let a = prompt("Isgaldan azad olunmus rayonumuz adini daxil edin!")
+
+switch (a) {
+    case "Cebrayil":
+        console.log("Cebrayi rayonu 04.10.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Fuzuli":
+        console.log("Fuzuli rayonu 17.10.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Zengilan":
+        console.log("Zengilan rayonu 20.10.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Qubadli":
+        console.log("Qubadli rayonu 25.10.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Susa":
+        console.log("Susa seheri 08.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Xankendi":
+        console.log("Xankendi seheri 10.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Xocali":
+        console.log("Xocali seheri 10.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Xocavend":
+        console.log("Xocavend seheri 10.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Agdere":
+        console.log("Agdere seheri 10.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Agdam":
+        console.log("Agdam seheri 20.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Kelbecer":
+        console.log("Kelbecer seheri 25.11.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+    case "Lacin":
+        console.log("Lacin seheri 01.12.2020-ci il tarixinde isgaldan azad edilib")
+        break;
+
+    default:
+        break;
+}
